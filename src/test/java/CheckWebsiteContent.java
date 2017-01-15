cbe459a7c1f0d790eaf9c971fc422cda63f78b28
Eric de With <eric.de.with@rabobank.nl>
2017-01-15 14:07:43 +0100

    Add integration test

--- src/test/java/CheckWebsiteContent.java
+++ src/test/java/CheckWebsiteContent.java
@@ -1,8 +1,12 @@
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -13,21 +17,28 @@ import static org.junit.Assert.assertTrue;
 public class CheckWebsiteContent {
     private static WebDriver driver;
 
-    @Test
-    public void checkWebsiteTitle(){
-//        WebdriverSetup webdriverSetup = new WebdriverSetup(driver);
+    @BeforeClass
+    public static void setUp(){
         DesiredCapabilities capabilities=DesiredCapabilities.firefox();
         capabilities.setCapability("marionette", false);
         driver = new FirefoxDriver(capabilities);
         driver.manage().window().maximize();
         driver.get("http://www.otwn.nl");
+    }
+
+    @Test
+    public void checkWebsiteTitle(){
+//        WebdriverSetup webdriverSetup = new WebdriverSetup(driver);
 
         driver.findElement(By.linkText("MAVEN")).click();
 
         String textOnPage = driver.findElement(By.className("single_page")).getText();
 
         assertTrue(textOnPage.contains("Bron: https://nl.wikipedia.org/wiki/Apache_Maven"));
+    }
 
+    @AfterClass
+    public static void tearDown(){
         driver.quit();
     }
 
